f168bebd8794d4c504398122792b5aedfa1911e5
Safei Ashraf <Safei.ashraf@gmail.com>
2020-08-05 07:59:38 +0200

    adding toggle logic to hamburger menu items

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,9 @@
 //Adding tooggle logic to display menu Icons:
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const toggler = document.querySelectorAll('.toggle')
+const navList = document.querySelector('.nav-list');
 
 hamburgerMenu.addEventListener('click', function(){
-    toggler.forEach( icon => { icon.classList.toggle('open')})
+    toggler.forEach( icon => { icon.classList.toggle('open')});
+    navList.classList.toggle('open');
 });
